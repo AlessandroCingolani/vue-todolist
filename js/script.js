93d72407d4a20556d7710ae0bea602338b601697
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-29 15:43:12 +0200

    [dd]:newTask and addTask function need fix push

--- js/script.js
+++ js/script.js
@@ -9,19 +9,32 @@ createApp({
           text:'Comprare la pizza',
           doneTask : false
         },
+
         {
           text:'Comprare latte',
           doneTask : false
         },
+
         {
           text:'Allenamento',
           doneTask : false
-        },
+        }
       ],
 
+      newTask : {
+        text:'',
+        doneTask: false
+      }
+      
+      
+    }
+  },
 
-
-
+  
+  methods : {
+    addTask(){
+      this.task.unshift(this.newTask);
+      this.newTask.text = '';
     }
   }
 
